a651477647ffc279f493cfdeee7d65f64a1a856d
Dipesh Dhote <dpsh.dhote@gmail.com>
2024-05-14 18:41:09 +0530

    add files

--- app.py
+++ app.py
@@ -1,4 +1,18 @@
+import sys
 from src.flight_price_prediction.logger import logging
+from src.flight_price_prediction.exception import CustomException
+from src.flight_price_prediction.components.data_ingestion import DataIngestion,DataIngestionConfig
 
 if __name__=="__main__":
     logging.info("logging has been started")
+    try:
+        data_ingetion = DataIngestion()
+        data_ingetion.initiate_data_ingestion()
+        
+    
+    
+    
+    
+    
+    except Exception as e:
+        raise CustomException(e,sys)
